33f64b76da9ac8fef2ad04f324e333fdb30aeeb6
mobayen <mobayen.mreza@gmail.com>
2022-11-21 23:30:33 -0500

    installs tailwindCSS and a font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 
+  modules: [],
+
+  css: [
+    '~/assets/css/main.css'
+  ],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
 })
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./app.vue",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: [
+          'Poppins',
+          'sans-serif'
+        ]
+      }
+    },
+  },
+  plugins: [],
+}
