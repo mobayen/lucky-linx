624e22c3869a8cc220543aefadf6da4282a192c4
mobayen <mobayen.mreza@gmail.com>
2022-11-22 00:33:57 -0500

    favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 
+  app: {
+    head: {
+      link: [
+        // TODO: it's more customizavle to move the file to the /assets dir
+        { rel: 'icon', type: 'image/png', href: '/ll-ico.png' }
+      ]
+    }
+  },
+
   modules: [],
 
   css: [
