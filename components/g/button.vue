1a95171d618eff08c2a3204671e03888fa887af4
mobayen <mobayen.mreza@gmail.com>
2022-11-25 00:08:05 -0500

    two generic HTML components

---
+++ components/g/button.vue
@@ -0,0 +1,25 @@
+<template>
+  <button
+    class="bg-gray-500 text-white/90 font-semibold rounded px-4 py-2"
+    :type="type"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+interface props {
+  type?: 'button' | 'reset' | 'submit'
+}
+
+withDefaults(
+  defineProps<props>(),
+  {
+    type: 'button'
+  }
+)
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/g/input.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <label v-if="label" class="block">
+      {{ label }}
+    </label>
+
+    <input
+      :type="type"
+      class="bg-white/80 rounded block w-full focus:outline-none p-2 text-lg"
+      @input="onInput"
+    >
+  </div>
+</template>
+
+<script setup lang="ts">
+
+// PROPS //
+// TODO: other types will be added as I go
+interface props {
+  modelValue?: string,
+  type: 'text' | 'password' | 'email',
+  label?: string
+}
+
+withDefaults(defineProps<props>(), {
+  modelValue: '',
+  type: 'text',
+  label: ''
+})
+
+// EMIT //
+interface emitType {
+  (e: 'input', elementEvent: Event): void,
+  (e: 'update:modelValue', val: string): void
+}
+const emit = defineEmits<emitType>()
+
+// METHODS/FUNCTIONS //
+function onInput ($event: InputEvent) {
+  // TODO: what are other types other than HTMLInputElement???
+  // console.log($event.target?.constructor.name ?? null)
+
+  if ($event.target instanceof HTMLInputElement) {
+    const value = $event.target.value
+
+    // enables two-way-binding
+    emit('update:modelValue', value)
+
+    // default input behaviour
+    emit('input', $event)
+  }
+}
+</script>
