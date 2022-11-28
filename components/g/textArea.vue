4ad0427239806295d48a0438d1de3c50057df76d
mobayen <mobayen.mreza@gmail.com>
2022-11-27 21:19:59 -0500

    generic textarea component

---
+++ components/g/textArea.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <label v-if="label" class="block text-gray-500/70 ml-2">
+      {{ label }}
+    </label>
+
+    <textarea
+      class="mb-1 px-4 py-2 w-full block
+        border rounded focus:border-gray-400/80
+        focus:outline-none text-lg bg-gray-200/50 focus:bg-gray-200/80 "
+      :value="modelValue"
+      @input="onInput"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+// PROPS //
+// TODO: other types will be added as I go
+interface props {
+  modelValue?: string,
+  label?: string
+}
+
+withDefaults(defineProps<props>(), {
+  modelValue: '',
+  label: ''
+})
+
+// EMIT //
+interface emitType {
+  (e: 'input', elementEvent: Event): void,
+  (e: 'update:modelValue', val: string): void
+}
+const emit = defineEmits<emitType>()
+
+// METHODS/FUNCTIONS //
+function onInput ($event: InputEvent) {
+  // TODO: what are other types other than HTMLInputElement & HTMLTextAreaElement???
+
+  if ($event.target instanceof HTMLTextAreaElement) {
+    const value = $event.target.value
+
+    // enables two-way-binding
+    emit('update:modelValue', value)
+
+    // default input behaviour
+    emit('input', $event)
+  }
+}
+</script>
