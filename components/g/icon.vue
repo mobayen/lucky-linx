4f3614eed61207b7896c20111531b73afc483e6e
mobayen <mobayen.mreza@gmail.com>
2022-12-06 23:08:58 -0500

    icon component

---
+++ components/g/icon.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+withDefaults(defineProps<{
+  icon?: string;
+}>(), {
+  icon: 'blur_on'
+})
+</script>
+
+<template>
+  <span class="flex items-center justify-center material-icons-outlined ">
+    {{ icon }}
+  </span>
+</template>
