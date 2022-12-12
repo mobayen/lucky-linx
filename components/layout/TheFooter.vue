c0e126633b68d4f1e582ab4adc2eba7b84cd37a7
mobayen <mobayen.mreza@gmail.com>
2022-12-11 19:41:07 -0500

    modifies the footer

--- components/layout/TheFooter.vue
+++ components/layout/TheFooter.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div class="bg-yellow-300 text-gray-800/40 text-sm flex justify-center">
-    <div class="p-4 flex items-center w-luckylinx">
+    <div class="pt-4 pb-8 px-8 flex items-center w-luckylinx">
       LuckyLinx made with
       <gIcon icon="favorite" class="text-rose-600/60" />
       !
