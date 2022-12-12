0130adf8177050ae607c4a914fd973ef19614032
mobayen <mobayen.mreza@gmail.com>
2022-12-11 22:34:44 -0500

    footer colors

--- components/layout/TheFooter.vue
+++ components/layout/TheFooter.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="bg-yellow-300 text-gray-800/40 text-sm flex justify-center">
+  <div class="bg-yellow-300/20 text-gray-800/50 text-sm flex justify-center">
     <div class="pt-4 pb-8 px-8 flex items-center w-luckylinx">
       LuckyLinx made with
       <gIcon icon="favorite" class="text-rose-600/60" />
