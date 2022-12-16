e68f1a01df9df74fad41a5b7bea66effce9f84cc
mobayen <mobayen.mreza@gmail.com>
2022-12-15 23:56:23 -0500

    generic <pre> component

---
+++ components/g/pre.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bg-yellow-300 text-gray-700/80 text-xs py-1 px-3 rounded  overflow-auto">
+    <strong>{{ title }}</strong>
+    <pre class=""><slot /></pre>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface IProps {
+  title?: string
+}
+
+defineProps<IProps>()
+</script>
